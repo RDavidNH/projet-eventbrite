bad574522b65ce9d1fdca9512c2fe36a68c30aef
Rabedaoro David N H <rdavidnh@gmail.com>
2019-11-07 22:42:07 +0300

    added nested resources to events

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "events#index"
-  resources :events
+  resources :events do
+      resources :attendances
+  end
   resources :users
-  resources :attendances
+
 end
