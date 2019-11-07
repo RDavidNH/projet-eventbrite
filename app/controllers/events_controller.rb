c6acc62e68913d442d0af4c17b775d8962773983
Rabedaoro David N H <rdavidnh@gmail.com>
2019-11-07 11:33:29 +0300

    removed access restriction for index method/view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,18 @@
 class EventsController < ApplicationController
 
-  before_action :authenticate_user, only: [:index, :new, :create, :edit, :update]
-  
+  before_action :authenticate_user, only: [:new, :create, :edit, :update]
+
   def index
     @events = Event.all
   end
-  
+
   def new
     @new_event = Event.new
   end
-  
-  def create 
+
+  def create
     p params
-    
+
     @new_event = Event.new(
       title: params[:title],
       start_date: params[:start_date],
@@ -22,24 +22,24 @@ class EventsController < ApplicationController
       location: params[:location],
       user: current_user
     )
-    
+
     if @new_event.save
       redirect_to @new_event
     else
       render 'new'
     end
-    
+
   end
-  
+
   def show
     @event = Event.find(params[:id])
   end
-  
+
   def authenticate_user
     unless current_user
       flash[:danger] = "Please log in."
       redirect_to new_user_session_path
     end
   end
-  
+
 end
