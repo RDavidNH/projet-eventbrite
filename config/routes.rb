2f1afd61d9d3e77feb5d6594e36f66877733447e
Rabedaoro David N H <rdavidnh@gmail.com>
2019-11-07 15:26:45 +0300

    added attendances controller/routes/etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root "events#index"
   resources :events
   resources :users
+  resources :attendances
 end
