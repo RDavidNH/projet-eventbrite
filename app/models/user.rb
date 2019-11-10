f78effdf6619873decb46c25befd8eada45949a5
Rabedaoro David N H <rdavidnh@gmail.com>
2019-11-11 00:16:37 +0300

    removed whitespaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :created_events, class_name: "Event"
   has_many :attendances
   has_many :events, through: :attendances
